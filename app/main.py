36fb9d71e8e72879a6a4bf0777483954c7fef247
Evandro Sacramento II <evandro-dev@outlook.com>
2026-05-19 19:52:20 -0300

    adicionando o main.py

---
+++ app/main.py
@@ -0,0 +1,44 @@
+import schedule
+import time
+import threading
+from app.config import API_URLS, MONITORING_INTERVAL, WEBHOOK_URL, RESPONSE_TIME_THRESHOLD, FLASK_PORT
+from app.monitor import monitorar_listas
+from web import criar_app
+
+
+def loop_monitoramento():
+    
+    monitorar_listas(API_URLS)
+    schedule.every(MONITORING_INTERVAL).seconds.do(lambda: monitorar_listas(API_URLS))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    print("==============MONITOR DE APIs INICIADO===============")
+    print(F"API CONFIGURADAS: {len(API_URLS)}")
+    print(F"INTERVALO DE MONITORAMENTO: {MONITORING_INTERVAL} segundos")
+    print(F"THRESHOLD DE TEMPO DE RESPOSTA: {RESPONSE_TIME_THRESHOLD} ms")
+    print("=====================================================")
+
+
+    adicionar = input("Deseja adicionar mais APIs para monitorar? (s/n): ")
+    if adicionar.lower() == 's':
+        url_extra = input("Digite a URL da API a ser monitorada: ")
+        nome_extra = input("Digite o nome da API: ")
+        API_URLS.append({"name": nome_extra, "url": url_extra})
+        print(F"API {nome_extra} adicionada com sucesso!")
+
+
+    print("Iniciando monitoramento...")
+
+    t = threading.Thread(target=loop_monitoramento, daemon=True)
+
+    t.start()
+    app = criar_app()
+    print(F"Servidor web rodando na porta {FLASK_PORT}...")
+    app.run(host='0.0.0.0', port=FLASK_PORT, debug=False)    
+
+
+    
